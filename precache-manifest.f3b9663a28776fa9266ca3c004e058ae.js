c5f1464814f85be48d8ec85bea293b278d154757
Stefan Niewerth <stefan.niewerth@gmx.net>
2020-09-02 11:06:59 +0200

    Updates

---
+++ precache-manifest.f3b9663a28776fa9266ca3c004e058ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e42f7a9aea00ca14bd8669df4cf256a",
+    "url": "/sr-rework-creator/index.html"
+  },
+  {
+    "revision": "50dcbb49910502339242",
+    "url": "/sr-rework-creator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbdfbc8da1a12c0045cc",
+    "url": "/sr-rework-creator/static/js/2.4f118bad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sr-rework-creator/static/js/2.4f118bad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dcbb49910502339242",
+    "url": "/sr-rework-creator/static/js/main.6fad044e.chunk.js"
+  },
+  {
+    "revision": "ccb3532760e7697f3494",
+    "url": "/sr-rework-creator/static/js/runtime-main.a0710d13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sr-rework-creator/static/media/logo.5d5d9eef.svg"
+  }
+]);
