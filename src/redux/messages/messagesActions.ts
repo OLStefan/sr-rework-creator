ed375d912c01d4a87e3229a83429594890fbc65f
Stefan Niewerth <stefan.niewerth@gmx.net>
2020-09-20 10:40:22 +0200

    final messages reducer

--- src/redux/messages/messagesActions.ts
+++ src/redux/messages/messagesActions.ts
@@ -1,20 +1,46 @@
 export const ADD_ERROR_MESSAGE = 'addErrorMessage';
+export const REMOVE_ERROR_MESSAGE = 'removeErrorMessage';
 export const ADD_HINT_MESSAGE = 'addHintMessage';
+export const REMOVE_HINT_MESSAGE = 'removeHintMessage';
 
-interface AddErrorMessageAction {
+export interface AddErrorMessageAction {
 	type: typeof ADD_ERROR_MESSAGE;
-	card: string;
+	sectionName: string;
+	message: string;
 }
-export function addErrorMessage(cardName: string) {
-	return { type: ADD_ERROR_MESSAGE, cardName };
+export function addErrorMessage(sectionName: string, message: string): AddErrorMessageAction {
+	return { type: ADD_ERROR_MESSAGE, sectionName, message };
 }
 
-interface AddHintMessageAction {
+export interface RemoveErrorMessageAction {
+	type: typeof REMOVE_ERROR_MESSAGE;
+	sectionName: string;
+	message: string;
+}
+export function removeErrorMessage(sectionName: string, message: string): RemoveErrorMessageAction {
+	return { type: REMOVE_ERROR_MESSAGE, sectionName, message };
+}
+
+export interface AddHintMessageAction {
 	type: typeof ADD_HINT_MESSAGE;
-	card: string;
+	sectionName: string;
+	message: string;
 }
-export function addHintMessage(cardName: string) {
-	return { type: ADD_HINT_MESSAGE, cardName };
+export function addHintMessage(sectionName: string, message: string): AddHintMessageAction {
+	return { type: ADD_HINT_MESSAGE, sectionName, message };
 }
 
-export type MessagesAction = AddErrorMessageAction | AddHintMessageAction;
+export interface RemoveHintMessageAction {
+	type: typeof REMOVE_HINT_MESSAGE;
+	sectionName: string;
+	message: string;
+}
+export function removeHintMessage(sectionName: string, message: string): RemoveHintMessageAction {
+	return { type: REMOVE_HINT_MESSAGE, sectionName, message };
+}
+
+export type MessagesAction =
+	| AddErrorMessageAction
+	| RemoveErrorMessageAction
+	| AddHintMessageAction
+	| RemoveHintMessageAction;
